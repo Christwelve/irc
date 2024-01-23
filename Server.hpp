354ca913bf6b117fd0154d1a6c776aa9c3d47463
Christwelve <meng.christian28@gmail.com>
2024-01-23 18:45:21 +0100

    Server class, IRCError class main parsing implementation

--- Server.hpp
+++ Server.hpp
@@ -1,27 +1,43 @@
 #ifndef SERVER_HPP
 # define SERVER_HPP
 
-#define BUFFER_SIZE 1024
+# include <iostream>
+# include <string>
+# include <sys/socket.h>
+# include <netinet/in.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <poll.h>
+# include <vector>
+# include <signal.h>
+# include <cstddef>
+
+# define RED "\033[1;31m"
+# define GREEN "\033[1;32m"
+# define CLEAR "\033[0m"
+
+# define BUFFER_SIZE 1024
 
 
 class Server
 {
-    public:
-        Server(size_t port, char *password);	// Default Constructor
-        ~Server();								// Destructor
+	public:
+		static Server &getInstance(void);
 
-        initServer(size_t port, char *password);
-        runServer();
-        shutdownServer(int server_fd, std::vector<struct pollfd> clients);
+		~Server();						
 
-    private:
-        int server_fd_;
-        struct sockaddr_in address_;
-        std::vector<struct pollfd> clients_;
-        char *password_;
-        chat buffer_[BUFFER_SIZE];
+		void initServer(size_t port, const std::string &password);
+		void runServer(void);
+		void shutdownServer(void);
+		static void signalHandler(int signal);
 
-
+	private:
+		Server(void);	                        
+		size_t port_;
+		std::string password_;
+		struct sockaddr_in address_;
+		std::vector<struct pollfd> clients_;
+		char buffer_[BUFFER_SIZE];
 };
 
 #endif
