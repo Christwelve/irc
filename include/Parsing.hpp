010b41b88baf9e52a9e7e2f2c9f81698de71ddb7
Christwelve <meng.christian28@gmail.com>
2024-01-26 16:26:46 +0100

    map function pointer implementation

--- include/Parsing.hpp
+++ include/Parsing.hpp
@@ -7,104 +7,94 @@
 # include "Socket.hpp"
 # include "User.hpp"
 
-enum Command {
-	QUIT,
-	NICK,
-	USER,
-	JOIN,
-	PART,
-	PRIVMSG,
-	KICK,
-	INVITE,
-	TOPIC,
-	LIST,
-	MODE
-};
-
-std::map<std::string, Command> commandMap;
-void initializeCommandMap() {
-	commandMap["QUIT"] = QUIT;
-	commandMap["NICK"] = NICK;
-	commandMap["USER"] = USER;
-	commandMap["JOIN"] = JOIN;
-	commandMap["PART"] = PART;
-	commandMap["PRIVMSG"] = PRIVMSG;
-	commandMap["KICK"] = KICK;
-	commandMap["INVITE"] = INVITE;
-	commandMap["TOPIC"] = TOPIC;
-	commandMap["LIST"] = LIST;
-	commandMap["MODE"] = MODE;
-}
-
 
 void quit(User &user, std::vector<std::string> tokens)
 {
-
-	
+	(void)user;
 	std::cout << "quit() " << user.getNickname() << tokens[1] << std::endl;
 	//sendFuntion()
 	// :nickname!username@hostname QUIT :<quit message>\r\n
 	
 }	
 	
-// void nick(User &user, std::string newNickname)
 void nick(User &user, std::vector<std::string> tokens)
 {
-	
-	std::cout << "nick()" << std::endl;
+	(void)user;
+	std::cout << "nick() " << tokens[1] << std::endl;
 }
 
-// void userCmd(User &user, std::string rest)
 void userCmd(User &user, std::vector<std::string> tokens)
 {
-		
-	std::cout << "user() create or login a user" << std::endl;
-}
-// void join(User &user, std::string channel)
-void join(User &user, std::vector<std::string> tokens)
-{
-	std::cout << "join()" << std::endl;
-}
-// void part(User &user, std::string channel)
-void part(User &user, std::vector<std::string> tokens)
-{
-	std::cout << "part()" << std::endl;
-}
-// void privmsg(User &user, std::string reciever, std::string message)
-void privmsg(User &user, std::vector<std::string> tokens)
-{
-	std::cout << "privmsg()" << std::endl;
-}
-// void privmsg(User &user, std::string channel, std::string message)
-void privmsg(User &user, std::vector<std::string> tokens)
-{
-	std::cout << "privmsg()" << std::endl;
-}
-// void kick(User &user, std::string channel, std::string nickname, std::string reason)
-void kick(User &user, std::vector<std::string> tokens)
-{
-std::cout << "kick()" << std::endl;
-}
-// void invite(User &user, std::string nickname, std::string channel)
-void invite(User &user, std::vector<std::string> tokens)
-{
-	std::cout << "invite()" << std::endl;
-}
-// void topic(User &user, std::string channel, std::string newTopic)
-void topic(User &user, std::vector<std::string> tokens)
-{
-	std::cout << "topic()" << std::endl;
-}
-void list() 
-{
-	std::cout << "list()" << std::endl;
-}
-// void mode(std::string channel, std::string mode)
-void mode(std::vector<std::string> tokens)
-{
-	std::cout << "mode()" << std::endl;
+	(void)user;
+	std::cout << "user() "  << tokens[1] << tokens[2]  << tokens[3] << tokens[4] << std::endl;
 }
 
+void join(User &user, std::vector<std::string> tokens)
+{
+	(void)user;
+	std::cout << "join() " << tokens[1] << std::endl;
+}
+
+void part(User &user, std::vector<std::string> tokens)
+{
+	(void)user;
+
+	std::cout << "part() " << tokens[1] << std::endl;
+}
+
+void privmsg(User &user, std::vector<std::string> tokens)
+{
+	(void)user;
+	std::cout << "privmsg() " << tokens[1] << tokens[2] << std::endl;
+}
+
+void kick(User &user, std::vector<std::string> tokens)
+{
+	(void)user;
+	std::cout << "kick() " << tokens[1] << tokens[2] << tokens[3] << std::endl;
+}
+
+void invite(User &user, std::vector<std::string> tokens)
+{
+	(void)user;
+	std::cout << "invite() " << tokens[1] << tokens[2] << std::endl;
+}
+
+void topic(User &user, std::vector<std::string> tokens)
+{
+	(void)user;
+
+	std::cout << "topic() " << tokens[1] << tokens[2] << std::endl;
+}
+void list(User &user, std::vector<std::string> tokens) 
+{
+	(void)user;
+	(void)tokens;
+	std::cout << "list() " << std::endl;
+}
+
+void mode(User &user, std::vector<std::string> tokens)
+{
+	(void)user;
+
+	std::cout << "mode() " << tokens[1] << tokens[2] << std::endl;
+}
+
+typedef void (*CommandFunc)(User&, std::vector<std::string>);
+std::map<std::string, CommandFunc> commandMap;
+void initializeCommandMap() {
+	commandMap["QUIT"] = quit;
+	commandMap["NICK"] = nick;
+	commandMap["USER"] = userCmd;
+	commandMap["JOIN"] = join;
+	commandMap["PART"] = part;
+	commandMap["PRIVMSG"] = privmsg;
+	commandMap["KICK"] = kick;
+	commandMap["INVITE"] = invite;
+	commandMap["TOPIC"] = topic;
+	commandMap["LIST"] = list;
+	commandMap["MODE"] = mode;
+}
 
 std::string addSpaceAfterColon(std::string str) {
     size_t pos = str.find(':');
@@ -135,63 +125,10 @@ void inputParsing(User &user, std::string input)
 
 	std::transform(tokens[0].begin(), tokens[0].end(), tokens[0].begin(), ::toupper);
 
-	int commandValue = commandMap[tokens[0]];
-	std::istringstream iss;
-	std::string receiver, message;
-	std::string channel, nickname, reason;
-	switch (commandValue) {
-		case QUIT:
-			quit(user, tokens); 
-			break;
-		// case NICK:
-		// 	nick(user, rest);
-		// 	break;
-		// case USER:
-		// 	userCmd(user, rest);
-		// 	break;
-		// case JOIN:
-		// 	join(user, rest);
-		// 	break;
-		// case PART:
-		// 	part(user, rest);
-		// 	break;
-		// case PRIVMSG:
-		// 	iss.str(rest);
-		// 	iss >> receiver;
-		// 	getline(iss, message);
-		// 	message = message.substr(1);
-		// 	privmsg(user, receiver, message);
-		// 	break;
-		// case KICK:
-		// 	iss.str(rest);
-		// 	iss >> channel >> nickname;
-		// 	getline(iss, reason);
-		// 	reason = reason.substr(1); 
-		// 	kick(user, channel, nickname, reason);
-		// 	break;
-		// case INVITE:
-		// 	iss.str(rest);
-		// 	iss >> nickname >> channel;
-		// 	invite(user, nickname, channel);
-		// 	break;
-		// case TOPIC:
-		// 	iss.str(rest);
-		// 	iss >> channel;
-		// 	getline(iss, message);
-		// 	message = message.substr(1); 
-		// 	topic(user, channel, message);
-		// 	break;
-		// case LIST:
-		// 	list();
-		// 	break;
-		// case MODE:
-		// 	iss.str(rest);
-		// 	iss >> channel >> message;
-		// 	mode(channel, message);
-		// 	break;            
-		default:
-			std::cout << ":server 421 client :Unknown command " << tokens[0] << std::endl;
-			break;
+    std::map<std::string, CommandFunc>::iterator it = commandMap.find(tokens[0]);
+    if (it != commandMap.end()) {
+        it->second(user, tokens);
+    } else {
+        std::cout << ":server 421 client :Unknown command " << tokens[0] << std::endl;
 	}
 }
-
--- src/main.cpp
+++ src/main.cpp
@@ -6,64 +6,58 @@
 # include <sstream>
 
 
-// int parsing(int argc, char **argv) {
-// 	if (argc != 3)
-// 		throw IRCError("Usage: ./irc <port> <password>");
+int parsing(int argc, char **argv) {
+	if (argc != 3)
+		throw IRCError("Usage: ./irc <port> <password>");
 
-// 	std::stringstream ss(argv[1]);
-// 	int port;
+	std::stringstream ss(argv[1]);
+	int port;
 
-// 	if (!(ss >> port) || port < 0 || port > 65535 || !ss.eof())
-// 		throw IRCError(std::string("Invalid port number ") + argv[1]);
+	if (!(ss >> port) || port < 0 || port > 65535 || !ss.eof())
+		throw IRCError(std::string("Invalid port number ") + argv[1]);
 
-// 	return (port);
-// }
-
-// int main(int argc, char **argv)
-// {
-	// try
-	// {
-	// 	int port = parsing(argc, argv);
-
-	// 	std::cout << "port: " << port << std::endl;
-	// 	std::cout << "password: " << argv[2] << std::endl;
-	// 	Server &server = Server::getInstance();
-	// 	server.initServer(port, argv[2]);
-	// 	server.runServer();
-	// }
-	// catch (const std::exception &e)
-	// {
-	// 	std::cerr << e.what() << std::endl;
-	// 	return (1);
-	// }
-	// return (0);
-// }
-
-int main(void) {
-	Socket socket;
-	User user(socket);
-
-    initializeCommandMap();
-
-    // Test the inputParsing function with some test inputs
-	std::cout << "Test 1" << std::endl;
-    inputParsing(user, "qUiT :quitmessage");
-	// std::cout << "Test 2" << std::endl;
-    // inputParsing(user, "QUIT : \r\n");
-	// std::cout << "Test 3" << std::endl;
-    // inputParsing(user, "QUIT \r\n");
-	// std::cout << "Test 4" << std::endl;
-    // inputParsing(user, "QUI\r\n");
-    // inputParsing(user, "NICK newNickname\r\n");
-    // inputParsing(user, "USER username hostname servername :realname\r\n");
-    // inputParsing(user, "JOIN #channel\r\n");
-    // inputParsing(user, "PART #channel\r\n");
-    // inputParsing(user, "PRIVMSG receiver :message\r\n");
-    // inputParsing(user, "KICK #channel nickname :reason\r\n");
-    // inputParsing(user, "INVITE nickname #channel\r\n");
-    // inputParsing(user, "TOPIC #channel :newTopic\r\n");
-    // inputParsing(user, "LIST\r\n");
-    // inputParsing(user, "MODE #channel +o\r\n");
-
-    return (0);
+	return (port);
 }
+
+int main(int argc, char **argv)
+{
+	try
+	{
+		int port = parsing(argc, argv);
+
+		std::cout << "port: " << port << std::endl;
+		std::cout << "password: " << argv[2] << std::endl;
+		Server &server = Server::getInstance();
+		server.initServer(port, argv[2]);
+		server.runServer();
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+		return (1);
+	}
+	return (0);
+}
+
+// int main(void) {
+// 	Socket socket;
+// 	User user(socket);
+
+//     initializeCommandMap();
+
+
+//     inputParsing(user, "qUiT :quitmessage");
+//     inputParsing(user, "NICK newNickname");
+//     inputParsing(user, "USER username hostname servername :realname");
+//     inputParsing(user, "JOIN #channel");
+//     inputParsing(user, "PART #channel");
+//     inputParsing(user, "PRIVMSG receiver :message");
+//     inputParsing(user, "PRIVMSG #channel :message");
+//     inputParsing(user, "KICK #channel nickname :reason");
+//     inputParsing(user, "INVITE nickname #channel");
+//     inputParsing(user, "TOPIC #channel :newTopic");
+//     inputParsing(user, "LIST");
+//     inputParsing(user, "MODE #channel +o");
+
+//     return (0);
+// }
